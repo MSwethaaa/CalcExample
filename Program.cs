729e879e047bc621351f4dac35b4e2604548aae4
unknown <Swetha>
2024-02-16 13:12:31 +0530

    Firstcommit

--- Program.cs
+++ Program.cs
@@ -5,4 +5,9 @@ Console.WriteLine("Add Call");
 calc.Add(12, 45);
 Console.WriteLine("Multiply Call");
 calc.Multi(10.5, 12.45);
+Console.WriteLine("Division Call");
+calc.Div(20, 4);
+Console.WriteLine("Sub Call");
+calc.Sub(20, 4);
+
 
